ac07a1e278e1685fb040e96a3a28c493eb0944bc
Naveen Joy <naveenjoy18@gmail.com>
2024-10-15 23:21:06 +0530

    code update v2

---
+++ morning sessions/ConsoleApp2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter A");
+            string num1 = Console.ReadLine();
+            Console.WriteLine("Enter B");
+            string num2 = Console.ReadLine();
+            Console.WriteLine("Enter C");
+            string num3 = Console.ReadLine();
+
+            int a = Convert.ToInt32(num1);
+            int b = Convert.ToInt32(num2);
+            int c = Convert.ToInt32(num3);
+            int largest = a;
+            int secLargest = b;
+
+            if (b > a)
+            {
+                largest = b;
+                secLargest = a;             
+            }
+            if (c > secLargest && c < largest)
+            {
+                secLargest = c;
+            }
+            else if (c > largest) {
+                secLargest = largest;
+                largest = c;
+            }
+            else if (a==b || a==c || b == c)
+            {
+                Console.WriteLine("Numbers are not distinct");
+            }
+            else
+            {
+                Console.WriteLine(secLargest + " is the second largest number");
+            }
+            Console.ReadLine();
+
+
+            //if (a > b && a > c)
+            //{
+            //    Console.WriteLine(a + ": A is greater");
+            //}
+            //else if (b > a && b > c)
+            //{
+            //    Console.WriteLine(b + ": B is greater");
+            //}
+            //else if (c > a && c > b)
+            //{
+            //    Console.WriteLine(c + ": C is greater");
+            //}            
+            //Console.ReadLine();
+
+
+
+
+            //Console.WriteLine("hello");
+            //int a = 1;
+            //int b = 3;
+            //string c = "5";
+            //int d = Convert.ToInt32(a);
+            //if (a == b)
+            //{
+            //    Console.WriteLine(3 * (a + b));
+            //}
+            //if(a != b)
+            //{
+            //    Console.WriteLine(a + b);
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ morning sessions/loops,switch/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //for (int i=1; i<=11; i+=2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //print star pattern
+            //Console.WriteLine("Enter no. of rows");
+            //int rows = Convert.ToInt32(Console.ReadLine());
+            //for (int i=0; i<rows; i++)
+            //{
+            //    for (int j=0; j<=i; j++)
+            //    {
+            //        Console.Write("* ");
+            //    }
+            //    Console.WriteLine("");
+            //}
+
+            //sum of 5 natural no
+            //int sum = 0;
+            //for (int i=1; i<=5; i++)
+            //{
+            //    sum += i;
+            //}
+            //Console.WriteLine(sum);
+
+            //number pattern
+            //int n = 1;
+            //for (int i = 1; i <= 4; i++)
+            //{
+            //    for (int j = 1; j <= 7; j++)
+            //    {
+
+            //    }
+            //}
+            int n = 1;
+            int rows = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(n + " ");
+                    n += 1;
+                }
+                Console.WriteLine("");
+            }
+
+
+            //for (int i = 0; i < 30; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        continue;
+            //    }
+            //    if (i > 19)
+            //    {
+            //        break;
+            //    }
+            //    Console.WriteLine(i * 10);
+            //}
+
+            //for (int i = 10; i < 200; i++)
+            //{
+            //    if (i % 20 == 0)
+            //    {
+            //        continue;
+            //    }
+            //    if (i > 190)
+            //    {
+            //        break;
+            //    }
+            //    if (i%10 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //Console.WriteLine("Enter a number");
+            //int number = Convert.ToInt32(Console.ReadLine());           
+            //switch (number%2)
+            //{
+            //    case 0:
+            //        Console.WriteLine($"{number} is an even number");
+            //        break;
+            //    case 1:
+            //        Console.WriteLine($"{number} is an odd number");
+            //        break;
+            //    default:
+            //        Console.WriteLine("error");
+            //        break;
+            //}
+
+            //int[] num = {1, 2, 3, 4, 5};
+            //for (int i=0; i<num.Length; i++)
+            //{
+            //    Console.WriteLine(num[i]);
+            //}
+
+            Console.ReadLine();
+
+        }
+    }
+}
